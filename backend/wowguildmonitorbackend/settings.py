72932e50dd6c0013e7b4060e7f8d87425f57ea34
Jin Lee <paskal3314@gmail.com>
2025-04-13 18:23:03 -0600

    Added: CORS_ALLOWED_ORIGINS

--- backend/wowguildmonitorbackend/settings.py
+++ backend/wowguildmonitorbackend/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -52,6 +53,11 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173"
 ]
 
 ROOT_URLCONF = "wowguildmonitorbackend.urls"
